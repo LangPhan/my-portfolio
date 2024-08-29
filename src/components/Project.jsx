1f7f478ef0cad89f230e330e96f6b8c53f81351e
LangPhan <langphankk@gmail.com>
2024-08-29 15:00:14 +0700

    Refactor Project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,37 +1,99 @@
 import { motion } from "framer-motion";
 import ProjectCard from "./ProjectCart";
-import BugTrackerImg from "../assets/Bug-tracker.png";
-import DrinkStoreImg from "../assets/drink-store.png";
-import AppStore from "../assets/app-store.png";
-import Band from "../assets/w3-school-band.png";
-import Phone from "../assets/electric-store.png";
 
-import React from "../assets/icons/React-Light.svg";
-import NodeJS from "../assets/icons/NodeJS-Light.svg";
-import Spring from "../assets/icons/Spring-Light.svg";
-import TailwindCSS from "../assets/icons/TailwindCSS-Light.svg";
+import AppStore from "../assets/app-store.png";
+import BugTrackerImg from "../assets/Bug-tracker.png";
+import DrinkShop from "../assets/drink-store.png";
+import Phone from "../assets/electric-store.png";
+import TDTUFIND from "../assets/tdtufind.png";
+import Band from "../assets/w3-school-band.png";
+
+import Bootstrap from "../assets/icons/Bootstrap.svg";
 import CSS from "../assets/icons/CSS.svg";
-import HTML from "../assets/icons/HTML.svg";
 import Figma from "../assets/icons/Figma-Light.svg";
+import HTML from "../assets/icons/HTML.svg";
+import Java from "../assets/icons/Java-Light.svg";
+import JavaScript from "../assets/icons/JavaScript.svg";
+import Maven from "../assets/icons/Maven-Light.svg";
 import MongoDB from "../assets/icons/MongoDB.svg";
 import MySQL from "../assets/icons/MySQL-Light.svg";
+import NodeJS from "../assets/icons/NodeJS-Light.svg";
 import PHP from "../assets/icons/PHP-Light.svg";
-import Maven from "../assets/icons/Maven-Light.svg";
-import Java from "../assets/icons/Java-Light.svg";
-import Bootstrap from "../assets/icons/Bootstrap.svg";
-import JavaScript from "../assets/icons/JavaScript.svg";
+import React from "../assets/icons/React-Light.svg";
+import Spring from "../assets/icons/Spring-Light.svg";
+import TailwindCSS from "../assets/icons/TailwindCSS-Light.svg";
 
 function Project() {
-  const roleProject1 = ["Front-end Dev"];
-  const techProject1 = [React, NodeJS, TailwindCSS, MongoDB];
-  const roleProject2 = ["Full-stack Dev", "Video Editor"];
-  const techProject2 = [Java, Spring, MySQL, Maven, Bootstrap];
-  const roleProject3 = ["Front-end Dev"];
-  const techProject3 = [HTML, CSS, JavaScript];
-  const roleProject4 = ["Full-stack Dev"];
-  const techProject4 = [PHP, HTML, CSS, Bootstrap, MySQL];
-  const roleProject5 = ["WireFrame", "Mock-up", "Prototype"];
-  const techProject5 = [Figma];
+  const projects = [
+    {
+      title: "tdt find",
+      image: TDTUFIND,
+      roles: ["FullStack Dev"],
+      technologies: [
+        React,
+        TailwindCSS,
+        MongoDB,
+        NodeJS,
+      ],
+    },
+    {
+      title: "drink shop web",
+      image: DrinkShop,
+      roles: ["FullStack Dev"],
+      technologies: [
+        Java,
+        Spring,
+        MySQL,
+        Maven,
+        Bootstrap,
+      ],
+    },
+    {
+      title: "bug tracker",
+      image: BugTrackerImg,
+      roles: ["Front-end Dev"],
+      technologies: [
+        React,
+        NodeJS,
+        TailwindCSS,
+        MongoDB,
+      ],
+    },
+    {
+      title: "app and game marketplace",
+      image: AppStore,
+      roles: ["Front-end Dev"],
+      technologies: [
+        PHP,
+        HTML,
+        CSS,
+        Bootstrap,
+        MySQL,
+      ],
+    },
+    {
+      title:
+        "band music full responsive",
+      image: Band,
+      roles: ["Personal Project"],
+      technologies: [
+        HTML,
+        CSS,
+        JavaScript,
+      ],
+    },
+    {
+      title: "phone store design",
+      image: Phone,
+      roles: [
+        "Wire Frame",
+        "Mock-up",
+        "Prototype",
+      ],
+      technologies: [Figma],
+    },
+  ];
+
   return (
     <motion.div
       initial={{ opacity: 0, y: -50 }}
@@ -44,42 +106,26 @@ function Project() {
         Project
       </h2>
       <h2 className="absolute top-[12%] uppercase text-md text-center gradientText tracking-[4px]">
-        Use arrow keys to move between projects
+        Use arrow keys to move between
+        projects
       </h2>
       <div
         className="mt-[30%] sm:mt-[14%] w-full flex space-x-5
       overflow-x-scroll p-10 snap-x snap-mandatory scroll"
       >
-        <ProjectCard
-          name="Drink Shop Web"
-          img={DrinkStoreImg}
-          roles={roleProject2}
-          techs={techProject2}
-        />
-        <ProjectCard
-          name={"Bug Tracker"}
-          img={BugTrackerImg}
-          roles={roleProject1}
-          techs={techProject1}
-        />
-        <ProjectCard
-          name={"Band Music Full Responsive"}
-          img={Band}
-          roles={roleProject3}
-          techs={techProject3}
-        />
-        <ProjectCard
-          name={"App Store Web"}
-          img={AppStore}
-          roles={roleProject4}
-          techs={techProject4}
-        />
-        <ProjectCard
-          name={"Phone Store Design"}
-          img={Phone}
-          roles={roleProject5}
-          techs={techProject5}
-        />
+        {projects.map((project) => {
+          return (
+            <ProjectCard
+              key={project.name}
+              name={project.title}
+              img={project.image}
+              roles={project.roles}
+              techs={
+                project.technologies
+              }
+            />
+          );
+        })}
       </div>
     </motion.div>
   );
